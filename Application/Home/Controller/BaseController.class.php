66f6649dc09549534a77edb50a20e08c9574865a
mowei <895078501@qq.com>
2016-09-20 20:51:25 +0800

    login.html of css,js,img complete

---
+++ Application/Home/Controller/BaseController.class.php
@@ -0,0 +1,14 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class BaseController extends Controller {
+   public function __construct()
+   {
+   		parent::__construct();
+		$admin_username=session('admin_username');
+   		if(empty($admin_username))
+   		{
+   			echo '请先登录';exit;
+   		}
+   }
+}
